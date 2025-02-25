ce6ce1a8d554641143ccb4673836cc30cd6deb5b
wsyyyue777 <3023644716@qq.com>
2025-02-25 20:56:54 +0800

    移动端适配

--- client/src/components/Card/Card.js
+++ client/src/components/Card/Card.js
@@ -4,8 +4,9 @@ import { Link } from 'react-router-dom'
 
 const Card = ({ item, type }) => {
     return (
-        <Link to={`/product/?id=${item.id}`}>
-            <div className={classes.card}>
+
+        <div className={classes.card}>
+            <Link to={`/product/?id=${item.id}`}>
                 <div className={classes.image}>
                     <img src={item.img1} alt='' />
                 </div>
@@ -14,8 +15,9 @@ const Card = ({ item, type }) => {
                     <h3>￥{item.oldprice}</h3>
                     <h3>{item.newprice}</h3>
                 </div>
-            </div>
-        </Link>
+            </Link>
+        </div>
+
     )
 }
 
--- client/src/pages/Products/Products.js
+++ client/src/pages/Products/Products.js
@@ -1,13 +1,15 @@
-import React from 'react'
+import React, { useState } from 'react'
 import classes from './Products.module.css'
 import ProductsList from '../../components/ProductsList/ProductsList';
 import { useSearchParams } from 'react-router-dom';
 import Search from '../../components/Search/Search';
 import { useFilters } from '../../hooks/useFilters';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faFilter } from '@fortawesome/free-solid-svg-icons';
 const Products = () => {
     const [searchParams] = useSearchParams();
     const { filters, handleFilterChange } = useFilters(searchParams);
-
+    const [showFilter, setShowFilter] = useState(false);
 
     return (
         <div className={classes.products}>
@@ -16,53 +18,59 @@ const Products = () => {
             </div>
             <div className={classes.main}>
                 <div className={classes.left}>
-                    <div className={classes.filter}>
-                        <h2>商品种类</h2>
-                        {['FENDI', 'IWC', '报喜鸟', 'BELLE', '传祺', '日用', '食品'].map(category => (
-                            <div className={classes.filterInput}>
-                                <input
-                                    type='checkbox'
-                                    id={category}
-                                    value={category}
-                                    onChange={(e) => handleFilterChange('categories', e.target.value, e.target.checked)}
-                                />
-                                <label htmlFor={category}>{category}</label>
+                    <div className={classes.faFilter} onClick={() => setShowFilter(!showFilter)}>
+                        <FontAwesomeIcon icon={faFilter} />
+                    </div>
+                    {showFilter &&
+                        <div>
+                            <div className={classes.filter}>
+                                <h2>商品种类</h2>
+                                {['FENDI', 'IWC', '报喜鸟', 'BELLE', '传祺', '日用', '食品'].map(category => (
+                                    <div className={classes.filterInput}>
+                                        <input
+                                            type='checkbox'
+                                            id={category}
+                                            value={category}
+                                            onChange={(e) => handleFilterChange('categories', e.target.value, e.target.checked)}
+                                        />
+                                        <label htmlFor={category}>{category}</label>
+                                    </div>
+                                ))}
                             </div>
-                        ))}
-                    </div>
-                    <div className={classes.filter}>
-                        <h2>价格区间</h2>
-                        <div className={classes.filterInput}>
-                            <span>0</span>
-                            <input type='range' min={0} max={2000} value={filters.priceRange} onChange={e => {
-                                handleFilterChange('priceRange', e.target.value)
-                            }} />
-                            <span>{filters.priceRange}</span>
-                        </div>
-                    </div>
-                    <div className={classes.filter}>
-                        <h2>排序方式</h2>
-                        {['normal', 'minfirst'].map(sortType => (
-                            <React.Fragment key={sortType}>
-                                <input
-                                    type='radio'
-                                    id={sortType}
-                                    value={sortType}
-                                    name='sort'
-                                    onChange={e => {
-                                        handleFilterChange('sortBy', e.target.value)
-                                    }}
-                                    checked={filters.sortBy === sortType}
-                                />
-                                <label
-                                    htmlFor={sortType}
-                                    className={sortType === 'normal' ? classes.space : undefined}
-                                >
-                                    {sortType === 'normal' ? '综合' : '价格从低到高'}
-                                </label>
-                            </React.Fragment>
-                        ))}
-                    </div>
+                            <div className={classes.filter}>
+                                <h2>价格区间</h2>
+                                <div className={classes.filterInput}>
+                                    <span>0</span>
+                                    <input type='range' min={0} max={2000} value={filters.priceRange} onChange={e => {
+                                        handleFilterChange('priceRange', e.target.value)
+                                    }} />
+                                    <span>{filters.priceRange}</span>
+                                </div>
+                            </div>
+                            <div className={classes.filter}>
+                                <h2>排序方式</h2>
+                                {['normal', 'minfirst'].map(sortType => (
+                                    <React.Fragment key={sortType}>
+                                        <input
+                                            type='radio'
+                                            id={sortType}
+                                            value={sortType}
+                                            name='sort'
+                                            onChange={e => {
+                                                handleFilterChange('sortBy', e.target.value)
+                                            }}
+                                            checked={filters.sortBy === sortType}
+                                        />
+                                        <label
+                                            htmlFor={sortType}
+                                            className={sortType === 'normal' ? classes.space : undefined}
+                                        >
+                                            {sortType === 'normal' ? '综合' : '价格从低到高'}
+                                        </label>
+                                    </React.Fragment>
+                                ))}
+                            </div>
+                        </div>}
                 </div>
                 <div className={classes.right}>
                     <ProductsList filters={filters} />
